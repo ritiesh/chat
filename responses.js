cdaa9288e1ca1903242441ef1a98d8d8b4278e8f
RITIESH VARATHARAJAN <91539605+ritiesh@users.noreply.github.com>
2023-03-10 17:40:15 +0530

    Update responses.js

--- responses.js
+++ responses.js
@@ -1,14 +1,13 @@
-let array = ["if you want to confirm return type 'y' else 'no'", "Describe the problem with the product"]
+let array = ["if you want to confirm return type 'yes' else 'no'", "Describe the problem with the product"]
 let i = array.length;
 function getBotResponse(input) {
   if (i > 0) return array[--i];
   else {
-    if (input == 'y') {
+    if (input == "yes") {
       return "return initiated successfully";
     }
     else {
- 
-      return firstBotMessage();
+      return "Order return has been cancelled";
     }
   }
 }
